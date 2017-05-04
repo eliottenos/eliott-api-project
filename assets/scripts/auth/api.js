22e0da33001a365f4276ac80c81667f88e040b03
eliott enos <eliottenos@gmail.com>
2017-05-04 15:01:41 -0400

    initial deploy
    added initial html/scss

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const config = require('../config.js')
+const store = require('../store.js')
+
+const signUp = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-up/',
+    method: 'POST',
+    data
+    // data: data
+  })
+}
+
+const signIn = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-in/',
+    method: 'POST',
+    data
+    // data: data
+  })
+}
+
+const signOut = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-out/' + store.user.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+    // data: data
+  })
+}
+
+const changePassword = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/change-password/' + store.user.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
